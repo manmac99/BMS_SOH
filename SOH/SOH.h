009d68df966d9e16d9cd2729217a310f081a704a
Jinwoo Park <105034091+manmac99@users.noreply.github.com>
2024-11-29 00:48:57 +0900

    Add Impedance Based SOH

--- SOH/SOH.h
+++ SOH/SOH.h
@@ -3,11 +3,13 @@
 
 #include "SOH_Capacity.c"
 #include "SOH_Resistance.c"
+#include "SOH_Impedance.c"
 #include "SOH_Self_Discharge.c"
 #include "SOH.c"
 
 double SOH_Capacity(double original_Capacity, double charged_Data[]);
 double SOH_Resistance(double original_Resistance, double current_Resistance[]);
+double SOH_Impedance(double initial_Impedance, double impedance_Measurements[], double weights[]);
 double SOH_Self_Discharge(double initialCapacity, double measuredCapacityAfterPeriod, double expectedSelfDischargeRate);
 double SOH(double original_Capacity, double current_Capacity);
 
