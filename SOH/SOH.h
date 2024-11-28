0c286100fc0213781cc1d0969d14016437223fb5
Jinwoo Park <105034091+manmac99@users.noreply.github.com>
2024-11-28 22:36:17 +0900

    Capacity_Based_SOH

--- SOH/SOH.h
+++ SOH/SOH.h
@@ -2,13 +2,9 @@
 #define SOH_H
 
 #include "SOH_Capacity.c"
-#include "SOH_Resistance.c"
-#include "SOH_Self_Discharge.c"
 #include "SOH.c"
 
 double SOH_Capacity(double original_Capacity, double charged_Data[]);
-double Current_Capacity_Resistance(double originalResistance, double currentResistance, double originalCapacity);
-double SOH_Self_Discharge(double initialCapacity, double measuredCapacityAfterPeriod, double expectedSelfDischargeRate);
 double SOH(double original_Capacity, double current_Capacity);
 
 #endif 
