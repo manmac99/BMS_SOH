2a615bd243fb384e8d383e3114cd4e3fe6777199
Jinwoo Park <bagjinu766@gmail.com>
2024-11-28 19:56:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "SOH\SOH_Capacity.c"
 #include"SOH\SOH_Resistance.c"
 #include"SOH\SOH.c"
+#include"SOH\SOH_Self_Discharge.c"
 #include<stdio.h>
 
 int main() {
@@ -12,14 +13,22 @@ int main() {
     double currentCapacity = Current_Capacity(originalCapacity, currentMeasurements, numMeasurements, measurementInterval);
     double soh = SOH(originalCapacity, currentCapacity);
 
+    //저항 기반
     double originalResistance = 20; // 옴에서 측정
     double currentResistance = 21; // 옴에서 측정
 
     double currentCapacityByResistance = Current_Capacity_Resistance(originalResistance, currentResistance, originalCapacity);
     double sohByResistance = SOH(originalCapacity, currentCapacityByResistance);
 
+    //자기 방전
+    double measuredCapacityAfterPeriod = 98.0; // 일정 기간 후 측정된 용량, 예를 들어 98 Ah
+    double expectedSelfDischargeRate = 5.0; // 새 배터리의 예상 자기방전율, 예를 들어 5%
+
+    double self_discharge = SOH_Self_Discharge(originalCapacity, measuredCapacityAfterPeriod, expectedSelfDischargeRate);
+
     printf("SOH (Capacity-Based): %.2f%%\n", soh);
-    printf("SOH (Resistance-Based): %.2f%%\n", currentCapacityByResistance*100);
+    printf("SOH (Resistance-Based): %.2f%%\n", currentCapacityByResistance);
+    printf("The State of Health (SOH) based on self-discharge is: %.2f%%\n", self_discharge);
 
     return 0;
 }
