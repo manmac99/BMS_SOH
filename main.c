8b1f53c6c3c2b883b6b1a3101d06db0dc26f6139
Jinwoo Park <105034091+manmac99@users.noreply.github.com>
2024-11-29 00:47:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main() {
     double initial_Impedance = 0.010; // 초기 임피던스, Ohms
     double impedance_Measurements[3] = {0.011, 0.012, 0.013}; // 구간별 임피던스 측정값, Ohms
-    double weights[3] = {0.3, 0.5, 0.2}; // 각 구간별 가중치
+    double weights[3] = {0.2, 0.6, 0.2}; // 각 구간별 가중치
 
     double soh = SOH_Impedance(initial_Impedance, impedance_Measurements, weights);
     printf("Impedence Based SOH: %.2f%%\n", soh);
