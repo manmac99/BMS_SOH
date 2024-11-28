552475c385e0955835551e2a449e683adcf670e2
Jinwoo Park <105034091+manmac99@users.noreply.github.com>
2024-11-29 00:18:03 +0900

    Update SOH.c

--- SOH/SOH.c
+++ SOH/SOH.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 double SOH(double original_Capacity, double current_Capacity) {
-    if (original_Capacity == 0) {
+    if (original_Capacity == 100.0) {
         return 0; // 원래 용량이 0이면, 계산 불가
     }
     double soh = (current_Capacity / original_Capacity) * 100.0;
