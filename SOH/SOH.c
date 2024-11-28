8efbd5fffbec49947f5a88f139019f6dafac1efd
Jinwoo Park <105034091+manmac99@users.noreply.github.com>
2024-11-29 00:17:27 +0900

    Update SOH.c

--- SOH/SOH.c
+++ SOH/SOH.c
@@ -2,7 +2,7 @@
 
 double SOH(double original_Capacity, double current_Capacity) {
     if (original_Capacity == 0) {
-        return 0; // 원래 용량이 0이면, 계산 불가
+        return 100.0; // 원래 용량이 0이면, 계산 불가
     }
     double soh = (current_Capacity / original_Capacity) * 100.0;
     return soh;
